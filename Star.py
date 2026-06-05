c3a7d1a0283f9a6819ec438468f3bcb493e75a93
Ishaan-coder-feb <goyalishaan007@gmail.com>
2026-06-05 17:52:07 +0530

    Add files via upload

---
+++ Star.py
@@ -0,0 +1,24 @@
+import turtle
+turtle.Screen().bgcolor("orange")
+t=turtle.Turtle()
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.penup()
+t.right(150)
+t.forward(70)
+t.right(90)
+t.pendown()
+t.forward(100)
+t.right(120)
+t.forward(100)
+t.right(120)
+t.forward(100)
+t.done()
+
+
+
+
+
---
+++ square..py
@@ -0,0 +1,7 @@
+import turtle
+turtle.Screen().bgcolor("yellow")
+t=turtle.Turtle()
+for _ in range(4):
+    t.forward(50)
+    t.right(90)   
+t.done()
